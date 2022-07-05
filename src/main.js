71e46aab8adb521c8ea0eb01d800b40961c97134
liliaimilk <2331691042@qq.com>
2022-07-05 16:16:45 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+//rem适配
+import 'amfe-flexible'
+// import router from './router'
+// import store from './store'
+
+//添加vant组件
+import { Button } from 'vant';
+// import './styles/index.less'
+
 
 Vue.config.productionTip = false
+
+Vue.use(Button)
 
 new Vue({
   render: h => h(App),
